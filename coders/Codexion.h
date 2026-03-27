b0dbae544e668008904b24f1752ecbb818af0498
haer00reh <haer-reh@student.1337.ma>
2026-03-27 20:14:27 +0100

    first commit

---
+++ coders/Codexion.h
@@ -0,0 +1,15 @@
+#ifndef CODEXION_H
+# define CODEXION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool str_checker(char *str);
+bool arg_checker(char **args);
+
+
+#endif
---
+++ coders/main.c
@@ -0,0 +1,13 @@
+#include "Codexion.h"
+
+
+
+int main(int ac, char **av)
+{
+    int (number_of_coders), (number_of_compiles_required), (time_to_burnout), (time_to_compile), (time_to_debug), (time_to_refactor), (dongle_cooldown);
+    char *schedule;
+
+
+arg_checker(av);
+
+}
