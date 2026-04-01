d56c73ddd4e5b440284c6ec8124be4fdd920c214
haer00reh <haer-reh@student.1337.ma>
2026-04-01 18:28:34 +0100

    fixes atol

--- coders/parse_tools.c
+++ coders/parse_tools.c
@@ -1,22 +1,122 @@
 #include "Codexion.h"
 
-char	*copy_word(char *str, int len)
-{
-	char	*s;
-	int		i;
 
-	i = 0;
-	s = malloc(sizeof(char) * (len + 1));
-	if (!s)
-		return (NULL);
-	s[len] = '\0';
-	while (*str && i < len)
+bool	arg_to_long(char *str, long *ret)
+{
+	*ret = ft_atol(str);
+
+	if (*ret < 0)
 	{
-		s[i] = str[i];
+		fprintf(stderr, "Error occured while checking, found a negative number\nPlease provide a positive number\n\nquitting...\n");
+		return false;
+	}
+	else
+	return true;
+}
+
+void	destroy_simulation_runtime(t_simulation *sim)
+{
+	pthread_mutex_destroy(&sim->print_mutex);
+	pthread_mutex_destroy(&sim->stop_mutex);
+	pthread_mutex_destroy(&sim->counter_mutex);
+}
+
+bool init_dongles(t_simulation *sim)
+{
+	int i;
+
+	sim->dongles = malloc(sizeof(t_dongle) * sim->number_of_coders);
+	if (!sim->dongles)
+	{
+		free(sim->coders);
+		return (false);
+	}
+	i = 0;
+	while (i < sim->number_of_coders)
+	{
+		sim->dongles[i].id = i + 1;
+		sim->dongles[i].in_use = false;
+		sim->dongles[i].available_at = 0;
+		pthread_mutex_init(&sim->dongles[i].mutex, NULL);
+		pthread_cond_init(&sim->dongles[i].cond, NULL);
 		i++;
 	}
-	return (s);
+	return (true);
 }
+
+bool init_coder(t_simulation *sim)
+{
+	int i;
+
+	sim->coders = malloc(sizeof(t_coder) * sim->number_of_coders);
+	if (!sim->coders)
+		return (false);
+	i = 0;
+	while (i < sim->number_of_coders)
+	{
+		sim->coders[i].id = i + 1;
+		sim->coders[i].thread = 0;
+		sim->coders[i].last_compile_start = 0;
+		sim->coders[i].compiles_done = 0;
+		sim->coders[i].left_dongle = &sim->dongles[i];
+		sim->coders[i].right_dongle = &sim->dongles[(i + 1) % sim->number_of_coders];
+		sim->coders[i].sim = sim;
+		i++;
+	}
+	return (true);
+}
+
+
+bool	init_simulation_from_args(t_simulation *sim, char **av)
+{
+	int	ret;
+
+	sim->coders = NULL;
+	sim->dongles = NULL;
+	sim->monitor_thread = 0;
+	sim->simulation_start_time = 0;
+	sim->stop = false;
+	sim->fifo_sequence = 0;
+	
+	ret = pthread_mutex_init(&sim->print_mutex, NULL);
+	if (ret != 0)
+		return (false);
+	ret = pthread_mutex_init(&sim->stop_mutex, NULL);
+	if (ret != 0)
+	{
+		pthread_mutex_destroy(&sim->print_mutex);
+		return (false);
+	}
+	ret = pthread_mutex_init(&sim->counter_mutex, NULL);
+	if (ret != 0)
+	{
+		pthread_mutex_destroy(&sim->print_mutex);
+		pthread_mutex_destroy(&sim->stop_mutex);
+		return (false);
+	}
+	if (!arg_to_long(av[1], &sim->number_of_coders))
+		return (destroy_simulation_runtime(sim), false);
+	if (!arg_to_long(av[2], &sim->number_of_compiles_required))
+		return (destroy_simulation_runtime(sim), false);
+	if (!arg_to_long(av[3], &sim->time_to_burnout))
+		return (destroy_simulation_runtime(sim), false);
+	if (!arg_to_long(av[4], &sim->time_to_compile))
+		return (destroy_simulation_runtime(sim), false);
+	if (!arg_to_long(av[5], &sim->time_to_debug))
+		return (destroy_simulation_runtime(sim), false);
+	if (!arg_to_long(av[6], &sim->time_to_refactor))
+		return (destroy_simulation_runtime(sim), false);
+	if (!arg_to_long(av[7], &sim->dongle_cooldown))
+		return (destroy_simulation_runtime(sim), false);
+	if (!init_coder(sim))
+		return (destroy_simulation_runtime(sim), false);
+	if (!init_dongles(sim))
+		return (destroy_simulation_runtime(sim), false);
+	sim->schedule = av[8];
+
+	return (true);
+}
+
 
 long	ft_atol(char *str)
 {
@@ -31,71 +131,17 @@ long	ft_atol(char *str)
 			sign = -1;
 		str++;
 	}
+	if (*str < '0' || *str > '9')
+		return 0;
 	while (*str)
 	{
-		if (!(*str < '9' && *str > '0'))
+		if (*str < '0' || *str > '9')
 			return 0;
 		nbr = (nbr * 10) + (*str - '0');
+		if ((sign == 1 && nbr > 2147483647)
+			|| (sign == -1 && nbr > 2147483648))
+			return 0;
 		str++;
 	}
 	return (nbr * sign);
-}
-
-int	count_words(char *str, char sep)
-{
-	int (counter), (i);
-	counter = 0;
-	i = 0;
-	while (str[i])
-	{
-		while (str[i] && str[i] == sep)
-			i++;
-		if (str[i])
-			counter++;
-		while (str[i] && (str[i] != sep))
-			i++;
-	}
-	return (counter);
-}
-
-void	free_split(char **split)
-{
-	int	i;
-
-	i = 0;
-	while (split[i])
-	{
-		free(split[i]);
-		i++;
-	}
-	return ;
-}
-
-char	**ft_split(char *str, char sep)
-{
-	char	**split;
-
-	int (words), (i), (len);
-	words = count_words(str, sep);
-	split = malloc(sizeof(char *) * (words + 1));
-	if (!split)
-		return (NULL);
-	i = 0;
-	while (*str)
-	{
-		while (*str && *str == sep)
-			str++;
-		if (*str == '\0')
-			break ;
-		len = 0;
-		while (*(str + len) && *(str + len) != sep)
-			len++;
-		split[i] = copy_word(str, len);
-		if (!split[i])
-			return (free_split(split), NULL);
-		i++;
-		str += len;
-	}
-	split[i] = NULL;
-	return (split);
 }
